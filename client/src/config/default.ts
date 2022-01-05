dd077de4cea6c13865f0a440e3a06099a8a4fa90
danil a <danil.a@fora-soft.com>
2022-01-05 17:56:28 +0300

    add docker-compose

--- client/src/config/default.ts
+++ client/src/config/default.ts
@@ -1,2 +1,4 @@
+import 'dotenv/config'
+
 export const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:4000";
 export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
